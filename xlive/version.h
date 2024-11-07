f799a7847e45d337bc5090a9a9dd53a41bd8d3df
Berthalamew <40469582+Berthalamew@users.noreply.github.com>
2024-11-06 23:41:33 -0500

    update version to 0.7

--- xlive/version.h
+++ xlive/version.h
@@ -12,8 +12,8 @@
 
 // DLL VERSION
 #define DLL_VERSION_MAJOR               0
-#define DLL_VERSION_MINOR               6
-#define DLL_VERSION_REVISION            9
+#define DLL_VERSION_MINOR               7
+#define DLL_VERSION_REVISION            0
 #define DLL_VERSION_BUILD				0
 
 #define DLL_VERSION            DLL_VERSION_MAJOR, DLL_VERSION_MINOR, DLL_VERSION_REVISION, DLL_VERSION_BUILD
