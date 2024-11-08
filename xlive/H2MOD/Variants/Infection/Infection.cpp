890c652431c88272bf46032eaffc9a1df2e76b3f
nukeulater <18323679+nukeulater@users.noreply.github.com>
2024-11-08 11:54:06 +0200

    fix player team switch during infection when players are all local split screen players

--- xlive/H2MOD/Variants/Infection/Infection.cpp
+++ xlive/H2MOD/Variants/Infection/Infection.cpp
@@ -65,9 +65,9 @@ void Infection::sendTeamChange()
 	{
 		int32 player_count = NetworkSession::GetPlayerCount();
 
+		int32 player_array_index = 0;
 		if (player_count > 0)
 		{
-			int32 player_array_index = 0;
 			datum player_indexes[k_maximum_players] = {};
 			e_game_team player_teams[k_maximum_players] = {};
 
@@ -78,7 +78,7 @@ void Infection::sendTeamChange()
 					e_game_team team = zombiePlayerIndex == i ? k_zombie_team : k_humans_team;
 					bool is_current_player_zombie = zombiePlayerIndex == i;
 
-					if (!NetworkSession::IsPlayerLocal(i))
+					//if (!NetworkSession::IsPlayerLocal(i))
 					{
 						player_indexes[player_array_index] = i;
 						player_teams[player_array_index++] = team;
@@ -89,7 +89,7 @@ void Infection::sendTeamChange()
 							is_current_player_zombie
 						);
 					}
-					else 
+					/*else 
 					{
 						if (!Memory::IsDedicatedServer())
 						{
@@ -99,10 +99,11 @@ void Infection::sendTeamChange()
 							user_interface_controller_update_network_properties(local_player->controller_index);
 							LOG_TRACE_GAME(L"[h2mod-infection] setting local player team index, infected?: {}", is_current_player_zombie);
 						}
-					}
+					}*/
 				}
 			}
-			NetworkSession::GetActiveNetworkSession()->switch_players_to_teams(player_indexes, NetworkSession::GetPlayerCount(), player_teams);
+
+			NetworkSession::GetActiveNetworkSession()->switch_players_to_teams(player_indexes, player_array_index, player_teams);
 		}
 	}
 }
