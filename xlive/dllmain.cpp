6d83dea51c28a0ce65c8aeeb9302830f6443a143
nukeulater <18323679+nukeulater@users.noreply.github.com>
2026-02-24 20:02:59 +0200

    fix crash in release mode

--- xlive/dllmain.cpp
+++ xlive/dllmain.cpp
@@ -39,7 +39,7 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpRese
 	{
 	case DLL_PROCESS_ATTACH:
 		hThis = hModule;
-		assert(InitializeXLiveModuleTable());
+		InitializeXLiveModuleTable();
 		InitH2Startup();
 		break;
 
